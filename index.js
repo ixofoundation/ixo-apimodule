0c754055babd188c0f6c177389cbf75988ebd7a5
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-01-03 15:48:52 +0200

    # Added generateMnemonic function.

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-exports.pingIxo = function() {
-  console.log("ixo function call successfull!!!");
+exports.pingIxo = function () {
+    console.log("ixo function call successfull!!!");
+}
+
+exports.generateMnemonic = function () {
+    var bip39 = require('bip39');
+    return bip39.generateMnemonic();
 }
