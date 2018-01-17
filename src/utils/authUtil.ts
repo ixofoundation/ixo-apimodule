285fe95efc95d0906381cd89e5b1b108d958a047
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-01-17 15:42:48 +0200

    # Removed web3 npm package and replaced it with the minified javascript version. # Updated version.

--- src/utils/authUtil.ts
+++ src/utils/authUtil.ts
@@ -1,20 +1,35 @@
 import {Promise} from "es6-promise";
 import {ICredentialProviderResult} from "../models";
 
-const Web3 = require("web3");
+var loadjs = require('loadjs');
+declare const Web3: any;
 
 export function getWeb3Instance(provider: any): Promise<ICredentialProviderResult> {
     return new Promise((resolve, reject) => {
-        if (provider) {
-            var credentialProvider = {
-                provider: provider,
-                credentialProviderInstance: new Web3(provider)
-            };
-            resolve(credentialProvider);
-        } else {
-            reject(new Error('Provider is null or empty!'));
-        }
+        loadjs('https://cdn.rawgit.com/ethereum/web3.js/develop/dist/web3.min.js', 'web3');
+        loadjs.ready('web3', {
+            success: function () {
+                if (typeof provider !== 'undefined') {
+                    var credentialProvider: ICredentialProviderResult = {
+                        provider: provider,
+                        credentialProviderInstance: new Web3(provider)
+                    };
+                    resolve(credentialProvider);
+                } else {
+                    var credentialProvider: ICredentialProviderResult = {
+                        provider: provider,
+                        credentialProviderInstance: new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
+                    };
+                    resolve(credentialProvider);
+                }
+            },
+            error: function (depsNotFound: any) {
+                reject(new Error(depsNotFound))
+            }
+        });
     });
+
+
 }
 
 
