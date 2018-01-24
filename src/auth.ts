198a56c15268614476e6454070d78455a1bc36a3
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-01-24 14:06:10 +0200

    # Changed return type for sign function to return new Promise.

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,5 @@
 import {getWeb3Instance} from './utils/authUtil';
+import {Promise}         from 'es6-promise';
 
 const Eth = require('ethjs');
 const ethUtil = require('ethereumjs-util');
@@ -10,11 +11,16 @@ class Auth {
     }
 
     sign(provider: any, dataToSign: any): Promise<string> {
-        var eth = new Eth(provider.currentProvider);
-        var account = provider.eth.accounts[0];
-        var msg = ethUtil.bufferToHex(new Buffer(JSON.stringify(dataToSign), 'utf8'));
-
-        return eth.personal_sign(msg, account);
+        return new Promise((resolve, reject) => {
+            if (provider && dataToSign) {
+                var eth = new Eth(provider.currentProvider);
+                var account = provider.eth.accounts[0];
+                var msg = ethUtil.bufferToHex(new Buffer(JSON.stringify(dataToSign), 'utf8'));
+                return resolve(eth.personal_sign(msg, account));
+            } else {
+                return reject(new Error(`Provider or data to sign is missing!`));
+            }
+        });
     }
 
 }
