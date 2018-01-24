aa9b3917f88ed6f127a6ed252543480a912bb2c6
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-01-24 13:01:36 +0200

    # Added signing function using metamask and eth utils.

--- src/auth.ts
+++ src/auth.ts
@@ -1,9 +1,22 @@
-import {getWeb3Instance} from "./utils/authUtil";
+import {getWeb3Instance} from './utils/authUtil';
+
+const Eth = require('ethjs');
+const ethUtil = require('ethereumjs-util');
 
 class Auth {
+
     getCredentialProvider(provider: any): any {
         return getWeb3Instance(provider);
     }
+
+    sign(provider: any, dataToSign: any): Promise<string> {
+        var eth = new Eth(provider.currentProvider);
+        var account = provider.eth.accounts[0];
+        var msg = ethUtil.bufferToHex(new Buffer(JSON.stringify(dataToSign), 'utf8'));
+
+        return eth.personal_sign(msg, account);
+    }
+
 }
 
 export default Auth;
