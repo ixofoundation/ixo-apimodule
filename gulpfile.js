3864e08514e0f5ed17c883de20967b8ca1927686
Nicolaas Vercuiel <nicolaas.vercuiel73@gmail.com>
2018-08-07 17:16:20 +0200

    # Added typed definitions generation for ixo-module.

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,41 @@
-var gulp = require("gulp");
-var ts = require("gulp-typescript");
-var tsProject = ts.createProject("tsconfig.json");
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
 var uglify = require('gulp-uglify-es').default;
-var pump = require('pump');
 var gulpSequence = require('gulp-sequence');
 
-gulp.task("ts-to-js", function () {
-    return tsProject.src()
-        .pipe(tsProject())
-        .js.pipe(gulp.dest("dist"));
+var tsProject = ts.createProject('tsconfig.json', {
+	declaration: true
 });
 
-gulp.task("uglify", function () {
-    return gulp.src('dist/**')
-        .pipe(uglify(/* options */))
-        .pipe(gulp.dest("dist/"));
+gulp.task('ts-to-js', function() {
+	return tsProject
+		.src()
+		.pipe(tsProject())
+		.js.pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', gulpSequence('ts-to-js', 'uglify'));
+gulp.task('definitions', function(done) {
+	var exec = require('child_process').exec;
+	var cmd = 'node ./node_modules/dts-generator/bin/dts-generator --baseDir dist --project ./ --out dist/index.d.ts';
+	exec(cmd, function(error, stdout, stderr) {
+		console.log(stdout);
+		console.log(stderr);
+		done();
+	});
+});
+
+gulp.task('ts-def', function() {
+	return tsProject
+		.src()
+		.pipe(tsProject())
+		.dts.pipe(gulp.dest('dist'));
+});
+
+gulp.task('uglify', function() {
+	return gulp
+		.src('dist/**')
+		.pipe(uglify(/* options */))
+		.pipe(gulp.dest('dist/'));
+});
+
+gulp.task('default', gulpSequence('ts-to-js', 'uglify', 'definitions'));
