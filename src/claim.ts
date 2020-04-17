91abd33e6fb35799cbd94fb60d9dd2018e4bf100
Miguel <miguel.dingli.15@um.edu.mt>
2020-04-17 10:56:54 +0200

    Added fundProject and constructJsonPartialSignRequest. Also cleaned up.

--- src/claim.ts
+++ src/claim.ts
@@ -1,22 +1,22 @@
-import { Signature } from './common/models';
-import { constructJsonSignRequest } from './common/util';
-import { sendPostJSON } from './utils/http';
+import {Signature} from './common/models';
+import {constructJsonSignRequest} from './common/util';
+import {sendPostJSON} from './utils/http';
 
 class Claim {
-	createClaim(data: any, signature: Signature, PDSUrl: string): Promise<any> {
-		const json = constructJsonSignRequest('submitClaim', 'submit_claim', signature, data);
-		return sendPostJSON(PDSUrl + 'api/request', json);
-	}
+  createClaim(data: any, signature: Signature, PDSUrl: string): Promise<any> {
+    const json = constructJsonSignRequest('submitClaim', 'submit_claim', signature, data);
+    return sendPostJSON(PDSUrl + 'api/request', json);
+  }
 
-	evaluateClaim(data: any, signature: Signature, PDSUrl: string): Promise<any> {
-		const json = constructJsonSignRequest('evaluateClaim', 'evaluate_claim', signature, data);
-		return sendPostJSON(PDSUrl + 'api/request', json);
-	}
+  evaluateClaim(data: any, signature: Signature, PDSUrl: string): Promise<any> {
+    const json = constructJsonSignRequest('evaluateClaim', 'evaluate_claim', signature, data);
+    return sendPostJSON(PDSUrl + 'api/request', json);
+  }
 
-	listClaimsForProject(data: any, signature: Signature, PDSUrl: string): Promise<any> {
-		const json = constructJsonSignRequest('listClaims', 'list_claim', signature, data);
-		return sendPostJSON(PDSUrl + 'api/request', json);
-	}
+  listClaimsForProject(data: any, signature: Signature, PDSUrl: string): Promise<any> {
+    const json = constructJsonSignRequest('listClaims', 'list_claim', signature, data);
+    return sendPostJSON(PDSUrl + 'api/request', json);
+  }
 }
 
 export default Claim;
--- src/project.ts
+++ src/project.ts
@@ -1,87 +1,97 @@
 require('es6-promise');
-import { Signature } from './common/models';
-import { constructJsonSignRequest, constructPublicJsonRequest } from './common/util';
+import {Signature} from './common/models';
+import {constructJsonPartialSignRequest, constructJsonSignRequest, constructPublicJsonRequest} from './common/util';
 import Config from './config';
-import { sendGetJSON, sendPostJSON } from './utils/http';
+import {sendGetJSON, sendPostJSON} from './utils/http';
+
 class Project {
-	config: Config;
-	constructor(config: Config) {
-		this.config = config;
-	}
+  config: Config;
 
-	listProjects(): Promise<any> {
-		return sendGetJSON(this.config.getBlockSyncUrl() + '/api/project/listProjects');
-	}
+  constructor(config: Config) {
+    this.config = config;
+  }
 
-	getProjectByProjectDid(projectDid: any): Promise<any> {
-		return sendGetJSON(this.config.getBlockSyncUrl() + '/api/project/getByProjectDid/' + projectDid);
-	}
+  listProjects(): Promise<any> {
+    return sendGetJSON(this.config.getBlockSyncUrl() + '/api/project/listProjects');
+  }
 
-	getProjectByUserDid(senderDid: any): Promise<any> {
-		const payload = { senderDid: senderDid };
-		return sendPostJSON(this.config.getBlockSyncUrl() + '/api/project/', constructPublicJsonRequest('listProjectBySenderDid', payload));
-	}
+  getProjectByProjectDid(projectDid: any): Promise<any> {
+    return sendGetJSON(this.config.getBlockSyncUrl() + '/api/project/getByProjectDid/' + projectDid);
+  }
 
-	createProject(data: any, signature: Signature, PDSUrl: string): Promise<any> {
-		const json = constructJsonSignRequest('createProject', 'create_project', signature, data);
-		return sendPostJSON(PDSUrl + 'api/request', json);
-	}
+  getProjectByUserDid(senderDid: any): Promise<any> {
+    const payload = {senderDid: senderDid};
+    return sendPostJSON(this.config.getBlockSyncUrl() + '/api/project/', constructPublicJsonRequest('listProjectBySenderDid', payload));
+  }
 
-	updateProjectStatus(data: any, signature: Signature, PDSUrl: string): Promise<any> {
-		const json = constructJsonSignRequest('updateProjectStatus', 'project_status', signature, data);
-		return sendPostJSON(PDSUrl + 'api/request', json);
-	}
+  createProject(data: any, signature: Signature, PDSUrl: string): Promise<any> {
+    const json = constructJsonSignRequest('createProject', 'create_project', signature, data);
+    return sendPostJSON(PDSUrl + 'api/request', json);
+  }
 
-	createPublic(source: any, PDSUrl: string) {
-		let srcParts = source.split(',');
-		let data = srcParts[1];
-		let contentType = srcParts[0].split(';');
-		contentType = contentType[0].split(':')[1];
+  updateProjectStatus(data: any, signature: Signature, PDSUrl: string): Promise<any> {
+    const json = constructJsonSignRequest('updateProjectStatus', 'project_status', signature, data);
+    return sendPostJSON(PDSUrl + 'api/request', json);
+  }
 
-		let payload = {
-			data: data,
-			contentType: contentType
-		};
+  fundProject(data: any, signature: Signature, PDSUrl: string): Promise<any> {
+    const json = constructJsonPartialSignRequest('fundProject', 'fund_project', signature, data);
+    return sendPostJSON(PDSUrl + 'api/request', json);
+  }
 
-		const json = constructPublicJsonRequest('createPublic', payload);
-		return sendPostJSON(PDSUrl + 'api/public', json);
-	}
+  createPublic(source: any, PDSUrl: string) {
+    let srcParts = source.split(',');
+    let data = srcParts[1];
+    let contentType = srcParts[0].split(';');
+    contentType = contentType[0].split(':')[1];
 
-	fetchPublic(key: any, PDSUrl: string) {
-		let payload = {
-			key: key
-		};
-		return new Promise((resolve, reject) => {
-			const json = constructPublicJsonRequest('fetchPublic', payload);
-			sendPostJSON(PDSUrl + 'api/public', json)
-				.then((response: any) => {
-					if (response.result.data) {
-						let obj = {
-							data: response.result.data,
-							contentType: response.result.contentType
-						};
-						resolve(obj);
-					} else {
-						reject(null);
-					}
-				})
-				.catch((error: any) => {
-					reject(error);
-				});
-		});
-	}
+    let payload = {
+      data: data,
+      contentType: contentType
+    };
 
-	generateWithdrawObjectJson = (data: any, signature: string, created: any) => {
-		return JSON.stringify({ payload: [{type: "project/WithdrawFunds", value: data}], signatures: [{ signatureValue: signature, created: created }] });
-	}
+    const json = constructPublicJsonRequest('createPublic', payload);
+    return sendPostJSON(PDSUrl + 'api/public', json);
+  }
 
-	payOutToEthWallet(data: any, signature: Signature): Promise<any> {
-		const { signatureValue, created } = signature;
-		const withdrawObjectJson = this.generateWithdrawObjectJson(data, signatureValue, created);
-		const withdrawObjectUppercaseHex = new Buffer(withdrawObjectJson).toString('hex').toUpperCase();
+  fetchPublic(key: any, PDSUrl: string) {
+    let payload = {
+      key: key
+    };
+    return new Promise((resolve, reject) => {
+      const json = constructPublicJsonRequest('fetchPublic', payload);
+      sendPostJSON(PDSUrl + 'api/public', json)
+        .then((response: any) => {
+          if (response.result.data) {
+            let obj = {
+              data: response.result.data,
+              contentType: response.result.contentType
+            };
+            resolve(obj);
+          } else {
+            reject(null);
+          }
+        })
+        .catch((error: any) => {
+          reject(error);
+        });
+    });
+  }
 
-		return sendGetJSON(this.config.getBlockSyncUrl() + '/api/blockchain/0x' + withdrawObjectUppercaseHex);
-	}
+  generateWithdrawObjectJson = (data: any, signature: string, created: any) => {
+    return JSON.stringify({
+      payload: [{type: "project/WithdrawFunds", value: data}],
+      signatures: [{signatureValue: signature, created: created}]
+    });
+  }
+
+  payOutToEthWallet(data: any, signature: Signature): Promise<any> {
+    const {signatureValue, created} = signature;
+    const withdrawObjectJson = this.generateWithdrawObjectJson(data, signatureValue, created);
+    const withdrawObjectUppercaseHex = new Buffer(withdrawObjectJson).toString('hex').toUpperCase();
+
+    return sendGetJSON(this.config.getBlockSyncUrl() + '/api/blockchain/0x' + withdrawObjectUppercaseHex);
+  }
 }
 
 export default Project;
