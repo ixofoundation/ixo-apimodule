069f1466ce6403b7b5ebb09e757bd546f08c803d
migueldingli1997 <migueldingli97@gmail.com>
2020-11-16 15:45:58 +0100

    Converted all test steps to functions.

--- test/demo.ts
+++ test/demo.ts
@@ -38,62 +38,22 @@ const BLOCKSYNC_URL = 'https://block_sync_pandora.ixo.world';
 const ixo = new Ixo(BLOCKSYNC_URL);
 let cryptoUtil = new CryptoUtil();
 
-function status_updated_to(new_status: string) {
-  return function () {
-    // One of the ways to ensure the project was updated is to search for it
-    ixo.project.getProjectByProjectDid(projectDid)
-      .then((response: any) => {
-
-        // We expect this to return the entire project, with status:"<THE_NEW_STATUS>"
-        console.log('Expected status: ' + new_status)
-        console.log('Project: ' + success(JSON.stringify(response, null, '\t')));
-        expect(response).to.not.equal(null);
-      })
-      .catch((result: Error) => {
-        console.log(error(result));
-      });
-  }
-}
-
-function update_project_status_to(new_status: string) {
-  return function () {
-    const msgUpdateProjectStatus = {
-      projectDid: projectDid,
-      status: new_status,
-      txnId: '1111111' // This does not seem to make a big difference
-    }
-
-    // Any further project status updates are in our hands, so let's update the status to PENDING
-    const signature = cryptoUtil.getSignatureForPayload(ixoDid1, msgUpdateProjectStatus)
-    ixo.project.updateProjectStatus(msgUpdateProjectStatus, signature, CELLNODE_URL)
-      .then((response: any) => {
-
-        // At this stage, we cannot know for sure whether or not the project was updated, but we can get an idea (error => definitely not created)
-        console.log('Project update response: ' + success(JSON.stringify(response, null, '\t')));
-        expect(response).to.not.equal(null);
-      })
-      .catch((result: Error) => {
-        console.log(error(result));
-      });
-  }
-}
-
-function register_user_did(ixoDid: ISovrinDidModel) {
+function registerUserDid(userIxoDid: ISovrinDidModel) {
   return function () {
     const msgAddDid = {
-      did: ixoDid.did,
-      pubKey: ixoDid.verifyKey
+      did: userIxoDid.did,
+      pubKey: userIxoDid.verifyKey
     }
 
     // Get signature data for did/AddDid
-    ixo.utils.getSignData(msgAddDid, "did/AddDid", ixoDid.verifyKey)
+    ixo.utils.getSignData(msgAddDid, "did/AddDid", userIxoDid.verifyKey)
       .then((response: any) => {
 
         // If the response contains the signature bytes and the fee proceed
         if (response.sign_bytes && response.fee) {
 
           // Sign the signature bytes using the ixoDid and submit using registerUserDid
-          const signature = cryptoUtil.getSignatureForSignBytes(ixoDid, response.sign_bytes)
+          const signature = cryptoUtil.getSignatureForSignBytes(userIxoDid, response.sign_bytes)
           ixo.user.registerUserDid(msgAddDid, signature, response.fee)
             .then((response: any) => {
 
@@ -130,12 +90,81 @@ function register_user_did(ixoDid: ISovrinDidModel) {
   }
 }
 
-function create_agent(agentIxoDid: ISovrinDidModel, role: string) {
+function createProject() {
+  // Sign and submit a project creation request to Cellnode
+  const signature = cryptoUtil.getSignatureForPayload(projectCreatorDid, projectData)
+  ixo.project.createProject(projectData, signature, CELLNODE_URL)
+    .then((response: any) => {
+      // At this stage, we cannot know for sure whether or not the project was created, but we can get an idea (error => definitely not created)
+      console.log('Project create response: ' + success(JSON.stringify(response, null, '\t')));
+    })
+    .catch((result: Error) => {
+      console.log(error(result));
+    });
+}
+
+function projectCreated() {
+  // One of the ways to ensure the project was created was to search for it
+  ixo.project.listProjects()
+    .then((response: any) => {
+
+      // The latest project created is the last project in the list, so that is where we expect our project to be.
+      // As an additional success check, the entry should have status:"CREATED", indicating that it was successfully
+      // ledgered onto the blockchain and that the on-chain project status was also successfully updated to "CREATED".
+      console.log('Project list: ' + success(JSON.stringify(response, null, '\t')));
+      expect(response).to.not.equal(null);
+    })
+    .catch((result: Error) => {
+      console.log(error(result));
+    });
+}
+
+function updateProjectStatusTo(newStatus: string) {
+  return function () {
+    const msgUpdateProjectStatus = {
+      projectDid: projectDid,
+      status: newStatus,
+      txnId: '1111111' // This does not seem to make a big difference
+    }
+
+    // Any further project status updates are in our hands, so let's update the status to PENDING
+    const signature = cryptoUtil.getSignatureForPayload(ixoDid1, msgUpdateProjectStatus)
+    ixo.project.updateProjectStatus(msgUpdateProjectStatus, signature, CELLNODE_URL)
+      .then((response: any) => {
+
+        // At this stage, we cannot know for sure whether or not the project was updated, but we can get an idea (error => definitely not created)
+        console.log('Project update response: ' + success(JSON.stringify(response, null, '\t')));
+        expect(response).to.not.equal(null);
+      })
+      .catch((result: Error) => {
+        console.log(error(result));
+      });
+  }
+}
+
+function projectStatusUpdatedTo(newStatus: string) {
+  return function () {
+    // One of the ways to ensure the project was updated is to search for it
+    ixo.project.getProjectByProjectDid(projectDid)
+      .then((response: any) => {
+
+        // We expect this to return the entire project, with status:"<THE_NEW_STATUS>"
+        console.log('Expected status: ' + newStatus)
+        console.log('Project: ' + success(JSON.stringify(response, null, '\t')));
+        expect(response).to.not.equal(null);
+      })
+      .catch((result: Error) => {
+        console.log(error(result));
+      });
+  }
+}
+
+function createAgent(agentIxoDid: ISovrinDidModel, agentRole: string) {
   return function () {
     const msgCreateAgent = {
       email: 'test@ixo.com',
       name: 'Test',
-      role: role,
+      role: agentRole,
       agentDid: agentIxoDid.did,
       projectDid: projectDid
     }
@@ -150,10 +179,10 @@ function create_agent(agentIxoDid: ISovrinDidModel, role: string) {
   }
 }
 
-function update_agent_status_to(agentIxoDid: ISovrinDidModel, role: string) {
+function updateAgentStatusTo(agentIxoDid: ISovrinDidModel, agentRole: string) {
   return function () {
     const msgUpdateAgentStatus = {
-      role: role,
+      role: agentRole,
       agentDid: agentIxoDid.did,
       projectDid: projectDid,
       status: "1"
@@ -169,6 +198,46 @@ function update_agent_status_to(agentIxoDid: ISovrinDidModel, role: string) {
   }
 }
 
+function agentsCreatedAndApproved() {
+  const listData = {projectDid: projectDid};
+
+  const signature = cryptoUtil.getSignatureForPayload(projectCreatorDid, listData)
+  ixo.agent.listAgentsForProject(listData, signature, CELLNODE_URL).then((response: any) => {
+    console.log('Agent list for Project: ' + success(JSON.stringify(response, null, '\t')));
+    expect(response.result).to.not.equal(null);
+  }).catch((result: Error) => {
+    console.log(error(result));
+  });
+}
+
+function createClaim() {
+  // Note: only projectDid is required. Other values (name, weight, claimid, ...) can be string/object/array/
+  const msgCreateClaim = {name: 'doggy bag', weight: '2kg', claimid: "123", projectDid: projectDid};
+
+  const signature = cryptoUtil.getSignatureForPayload(claimerIxoDid, msgCreateClaim)
+  ixo.claim.createClaim(msgCreateClaim, signature, CELLNODE_URL).then((response: any) => {
+    console.log('Claim create response: ' + success(JSON.stringify(response, null, '\t')));
+  }).catch((result: Error) => {
+    console.log(error(result));
+  });
+}
+
+function evaluateClaim() {
+  // Note: only status and projectDid are required. Other values (name, weight, claimid, ...) can be string/object/array/
+  const msgEvaluateClaim = {
+    claimId: "94519f45e2c4c82cf67b62416414341b8e2dded1e55056bfb82075b169f536cd",
+    status: '1',
+    projectDid: projectDid
+  };
+
+  const signature = cryptoUtil.getSignatureForPayload(evaluatorIxoDid, msgEvaluateClaim)
+  ixo.claim.evaluateClaim(msgEvaluateClaim, signature, CELLNODE_URL).then((response: any) => {
+    console.log('Claim evaluate response: ' + success(JSON.stringify(response, null, '\t')));
+  }).catch((result: Error) => {
+    console.log(error(result));
+  });
+}
+
 const projectCreatorDid = ixoDid1;
 const agent1IxoDid = ixoDid1;
 const agent2IxoDid = ixoDid2;
@@ -177,39 +246,13 @@ const evaluatorIxoDid = ixoDid2;
 
 describe('Demo', () => {
 
-  it('should register DID 1', register_user_did(ixoDid1))
-  it('should register DID 2', register_user_did(ixoDid2))
-  it('should register DID 3', register_user_did(ixoDid3))
+  it('should register DID 1', registerUserDid(ixoDid1))
+  it('should register DID 2', registerUserDid(ixoDid2))
+  it('should register DID 3', registerUserDid(ixoDid3))
 
-  it('should create new project', () => {
-    // Sign and submit a project creation request to Cellnode
-    const signature = cryptoUtil.getSignatureForPayload(projectCreatorDid, projectData)
-    ixo.project.createProject(projectData, signature, CELLNODE_URL)
-      .then((response: any) => {
-        // At this stage, we cannot know for sure whether or not the project was created, but we can get an idea (error => definitely not created)
-        console.log('Project create response: ' + success(JSON.stringify(response, null, '\t')));
-      })
-      .catch((result: Error) => {
-        console.log(error(result));
-      });
-  });
+  it('should create new project', createProject);
 
-  it('should return list of projects and confirm that the project was created', () => {
-
-    // One of the ways to ensure the project was created was to search for it
-    ixo.project.listProjects()
-      .then((response: any) => {
-
-        // The latest project created is the last project in the list, so that is where we expect our project to be.
-        // As an additional success check, the entry should have status:"CREATED", indicating that it was successfully
-        // ledgered onto the blockchain and that the on-chain project status was also successfully updated to "CREATED".
-        console.log('Project list: ' + success(JSON.stringify(response, null, '\t')));
-        expect(response).to.not.equal(null);
-      })
-      .catch((result: Error) => {
-        console.log(error(result));
-      });
-  });
+  it('should return list of projects and confirm that the project was created', projectCreated);
 
   it('should expect that the project DID has been set', () => {
     if (projectDid.length == 0) {
@@ -219,70 +262,34 @@ describe('Demo', () => {
     }
   })
 
-  it('should create agent 1', create_agent(agent1IxoDid, 'SA'));  // SA => claimer
-  it('should approve agent 1', update_agent_status_to(agent1IxoDid, 'SA')); // Only necessary if auto-approvals are off
+  it('should create agent 1', createAgent(agent1IxoDid, 'SA'));  // SA => claimer
+  it('should approve agent 1', updateAgentStatusTo(agent1IxoDid, 'SA')); // Only necessary if auto-approvals are off
 
-  it('should create agent 2', create_agent(agent2IxoDid, 'EA'));  // EA => evaluator
-  it('should approve agent 2', update_agent_status_to(agent2IxoDid, 'SA')); // Only necessary if auto-approvals are off
+  it('should create agent 2', createAgent(agent2IxoDid, 'EA'));  // EA => evaluator
+  it('should approve agent 2', updateAgentStatusTo(agent2IxoDid, 'SA')); // Only necessary if auto-approvals are off
 
-  it('should confirm that the agents were created', () => {
-    const listData = {projectDid: projectDid};
+  it('should confirm that the agents were created', agentsCreatedAndApproved)
 
-    const signature = cryptoUtil.getSignatureForPayload(projectCreatorDid, listData)
-    ixo.agent.listAgentsForProject(listData, signature, CELLNODE_URL).then((response: any) => {
-      console.log('Agent list for Project: ' + success(JSON.stringify(response, null, '\t')));
-      expect(response.result).to.not.equal(null);
-    }).catch((result: Error) => {
-      console.log(error(result));
-    });
-  })
+  it('should update project status to PENDING', updateProjectStatusTo("PENDING"));
+  it('should return the project with status updated to PENDING', projectStatusUpdatedTo("PENDING"));
 
-  it('should update project status to PENDING', update_project_status_to("PENDING"));
-  it('should return the project with status updated to PENDING', status_updated_to("PENDING"));
+  it('should update project status to FUNDED', updateProjectStatusTo("FUNDED"));
+  it('should return the project with status updated to FUNDED', projectStatusUpdatedTo("FUNDED"));
 
-  it('should update project status to FUNDED', update_project_status_to("FUNDED"));
-  it('should return the project with status updated to FUNDED', status_updated_to("FUNDED"));
-
-  it('should update project status to STARTED', update_project_status_to("STARTED"));
-  it('should return the project with status updated to STARTED', status_updated_to("STARTED"));
+  it('should update project status to STARTED', updateProjectStatusTo("STARTED"));
+  it('should return the project with status updated to STARTED', projectStatusUpdatedTo("STARTED"));
 
   // NB: if fees were set up for the project (by default no), the project should be appropriately funded for claims
-
-  it('should create new claim', () => {
-    // Note: only projectDid is required. Other values (name, weight, claimid, ...) can be string/object/array/
-    const msgCreateClaim = {name: 'doggy bag', weight: '2kg', claimid: "123", projectDid: projectDid};
-
-    const signature = cryptoUtil.getSignatureForPayload(claimerIxoDid, msgCreateClaim)
-    ixo.claim.createClaim(msgCreateClaim, signature, CELLNODE_URL).then((response: any) => {
-      console.log('Claim create response: ' + success(JSON.stringify(response, null, '\t')));
-    }).catch((result: Error) => {
-      console.log(error(result));
-    });
-  });
+  it('should create new claim', createClaim);
 
   // NB: if fees were set up for the project (by default no), the project should be appropriately funded for evaluations
+  it('should evaluate claim', evaluateClaim);
 
-  it('should evaluate claim', () => {
-    // Note: only status and projectDid are required. Other values (name, weight, claimid, ...) can be string/object/array/
-    const msgEvaluateClaim = {
-      claimId: "94519f45e2c4c82cf67b62416414341b8e2dded1e55056bfb82075b169f536cd",
-      status: '1',
-      projectDid: projectDid
-    };
-
-    const signature = cryptoUtil.getSignatureForPayload(evaluatorIxoDid, msgEvaluateClaim)
-    ixo.claim.evaluateClaim(msgEvaluateClaim, signature, CELLNODE_URL).then((response: any) => {
-      console.log('Claim evaluate response: ' + success(JSON.stringify(response, null, '\t')));
-    }).catch((result: Error) => {
-      console.log(error(result));
-    });
-  });
-
-  it('should update project status to STOPPED', update_project_status_to("STOPPED"));
-  it('should return the project with status updated to STOPPED', status_updated_to("STOPPED"));
+  it('should update project status to STOPPED', updateProjectStatusTo("STOPPED"));
+  it('should return the project with status updated to STOPPED', projectStatusUpdatedTo("STOPPED"));
 
   // TODO: need to fix bug (?) which does not allow us to go to PAIDOUT if there are zero funds
 
-  it('should update project status to PAIDOUT', update_project_status_to("PAIDOUT"));
-  it('should return the project with status updated to PAIDOUT', status_updated_to("PAIDOUT"));
+  it('should update project status to PAIDOUT', updateProjectStatusTo("PAIDOUT"));
+  it('should return the project with status updated to PAIDOUT', projectStatusUpdatedTo("PAIDOUT"));
 });
